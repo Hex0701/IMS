2d82d0af47119eab3a80fc1a47eb94a4c4e2c39d
aakash aman <aakash.aman@student.sl.on.ca>
2024-10-11 13:24:50 -0400

    initial comit

---
+++ server/ims_server/src/main/java/ims_server/ims_server/ImsServerApplication.java
@@ -0,0 +1,13 @@
+package ims_server.ims_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsServerApplication.class, args);
+	}
+
+}
---
+++ server/ims_server/src/test/java/ims_server/ims_server/ImsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ims_server.ims_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
